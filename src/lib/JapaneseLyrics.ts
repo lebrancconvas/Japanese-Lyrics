1f64dc0f13ef89b8ab82128b4b8975982116b591
lebrancconvas <lebrancconvas@gmail.com>
2022-02-24 09:54:13 +0700

    🎨 Improve Code Format.

--- src/lib/JapaneseLyrics.ts
+++ src/lib/JapaneseLyrics.ts
@@ -11,6 +11,7 @@ const GetLyric = async(artistid: number, trackid: number) => {
 	const trackcode = JLyric[artistid - 1].tracks[trackid - 1].trackcode;
 	const urlPath: String[] = ['artist', artistcode, `${trackcode}.html`];
 	let queryParam = '';
+	
 	urlPath.map(path => {
 		queryParam += `/${path}`;
 	})
@@ -35,17 +36,21 @@ const GetLyric = async(artistid: number, trackid: number) => {
 const PrintLyric = async(artistid: number, trackid: number) => {
 	let getLyric = await GetLyric(artistid, trackid);
 	let lyricList: String[] = getLyric.split('<br>');
+
 	const trackname = JLyric[artistid - 1].tracks[trackid - 1].track;
 	const artistname = JLyric[artistid - 1].artist; 
 	const artistcode = JLyric[artistid - 1].artistcode;
 	const trackcode = JLyric[artistid - 1].tracks[trackid - 1].trackcode;
 	const trackspotify = JLyric[artistid - 1].tracks[trackid - 1].links.spotify;
 	const trackyoutube = JLyric[artistid - 1].tracks[trackid - 1].links.youtube;
+
 	const url = `https://j-lyric.net/artist/${artistcode}/${trackcode}.html`;
 	let lyricText = `# [${trackname} - ${artistname}](${url})  \n## Information  \nTrack: ${trackname}  \nArtist: ${artistname}  \n## Link to Listen  \n[Youtube](${trackyoutube})  \n[Spotify](${trackspotify})  \n## Lyric  \n`;
+	
 	for(let i = 0; i < lyricList.length; i++) {
 		lyricText += `${lyricList[i]}  \n`;
 	}
+
 	fs.writeFileSync(`./data/output/${trackname} - ${artistname}.md`, lyricText, (err: unknown) => {
 		console.log(err);
 		console.log('File Writing Failed.');
